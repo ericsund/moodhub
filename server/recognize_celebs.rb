bf7219d810c47a09d91ab0ba5391e3903e8076c3
cwaffles <cwaffles@users.noreply.github.com>
2017-07-15 20:25:23 -0700

    updated with print support

--- server/recognize_celebs.rb
+++ server/recognize_celebs.rb
@@ -4,7 +4,7 @@ require 'base64'
 rekognition = Aws::Rekognition::Client.new
 #filePath = "/home/eric/Desktop/barack-obama-smile.jpg"
 filePath = "/home/eric/Desktop/smile-flickr.jpg"
-filePath = "/home/eric/Desktop/blank.png"
+# filePath = "/home/eric/Desktop/angry.jpg"
 
 data = File.open(filePath, "rb")
 
@@ -15,3 +15,5 @@ resp = rekognition.detect_faces({
   },
   attributes: ["ALL"], # accepts DEFAULT, ALL
 })
+
+puts resp.face_details[0].emotions[0]
