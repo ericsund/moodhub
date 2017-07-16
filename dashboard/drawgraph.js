82905cf9c16eb3abd517387f31956e24fd6ff233
flyash <harsh700ca@gmail.com>
2017-07-15 18:17:45 -0700

    done data handlers. They will fill arrays defined. Send to charts.js

--- dashboard/drawgraph.js
+++ dashboard/drawgraph.js
@@ -1,3 +1,9 @@
+var data = []
+var commit_data = []
+var labels = []
+
+
+// Creates x and y points for data
 function handleData(json) {
     for (i in json.users) {
       var point = {
@@ -9,10 +15,45 @@ function handleData(json) {
     console.log(data)
 }
 
+// Finds commits and adds them to commit_data array
+function handleCommits(json) {
+  for (i in json.users) {
+    if (json.users[i].Commit_ID != null) {
+      var point = {
+        'x':json.users[i].time,
+        'y':json.users[i].mood
+      }
+      commit_data.push(point);
+    }
+  }
+  console.log(commit_data)
+}
 
-var data = []
+// Creates labels for user data points that are 3600 seconds apart
+// Avoids over crowdiing x-axis with labels
+function handleLabels(json) {
+  // Sort json according time as labels are sorted by time.
+  json.users.sort(function(a,b) {
+    return a.time - b.time
+  })
+
+  var last_unix_time = -3600
+  for (i in json.users) {
+    var date = new Date (json.users[i].time*1000)
+    if (json.users[i].time - last_unix_time >= 3600) {
+      labels.push((date.getHours()+1) +":00");
+      last_unix_time = json.users[i].time
+    } else {
+      labels.push("")
+    }
+  }
+  console.log(labels)
+}
 
 
+
+
+// For testing purpose
 var fakeData = {
   'users':[
        {
@@ -24,65 +65,64 @@ var fakeData = {
        },
        {
            "user": 1,
-           "time": 15000030,
+           "time": 15004030,
            "mood": 0.23232,
            "Commit_ID": "Refactor",
            "RawResponse": null
        },
        {
            "user": 1,
-           "time": 15000040,
+           "time": 15004040,
            "mood": 0.3232,
            "Commit_ID": "Refactor",
            "RawResponse": null
        },
        {
            "user": 1,
-           "time": 15000060,
+           "time": 15010060,
            "mood": 0.992,
            "Commit_ID": "Refactor",
            "RawResponse": null
        },
        {
            "user": 1,
-           "time": 15000070,
+           "time": 15010070,
            "mood": 0.932,
            "Commit_ID": "Refactor",
            "RawResponse": null
        },
        {
            "user": 1,
-           "time": 15000080,
+           "time": 15013680,
            "mood": 0.932,
            "Commit_ID": "Refactor",
            "RawResponse": null
        },
        {
            "user": 1,
-           "time": 15000090,
+           "time": 15120090,
            "mood": 0.1432,
            "Commit_ID": "Refactor",
            "RawResponse": null
        },
        {
            "user": 1,
-           "time": 15000100,
+           "time": 15120100,
            "mood": 0.413112,
            "Commit_ID": "Refactor",
            "RawResponse": null
        },
        {
            "user": 1,
-           "time": 15000200,
+           "time": 15120200,
            "mood": 0.222,
            "Commit_ID": "Refactor",
            "RawResponse": null
        },
        {
            "user": 1,
-           "time": 15000300,
+           "time": 15120300,
            "mood": 0.234,
-           "Commit_ID": "Refactor",
            "RawResponse": null
 
        }
@@ -90,4 +130,5 @@ var fakeData = {
  }
 
 handleData(fakeData)
-console.log('hello')
+handleCommits(fakeData)
+handleLabels(fakeData)
